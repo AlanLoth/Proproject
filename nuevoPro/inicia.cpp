85fbc6d72ab76cb6c1e8c416421fb5c6b1ccb980
AlanLoth <alanloth@gmail.com>
2019-06-06 22:26:16 -0500

    Update inicia.cpp

--- nuevoPro/inicia.cpp
+++ nuevoPro/inicia.cpp
@@ -1,5 +1,6 @@
 #include "inicia.h"
 #include "ui_inicia.h"
+#include "mainwindow.h"
 
 Inicia::Inicia(QWidget *parent) :
     QDialog(parent),
@@ -16,16 +17,31 @@ Inicia::~Inicia()
 
 void Inicia::on_cancela_clicked()
 {
+    MainWindow* w=new MainWindow;
+    w->show();
     close();
 }
 
 void Inicia::on_okea_clicked()
 {
+    int x=0;
     QString pasw = ui->textEdit_2->toPlainText();
     QString mat = ui->textEdit->toPlainText();
     if (pasw=="" || mat==""){
         QMessageBox::information(this,"Error","Rellene todo");
     }else {
-            inic(mat,pasw);
+            x=inic(mat,pasw);
+            if (0==x){
+                QMessageBox::information(this,"Error","Usuario o pasword incorrecto");
+            } else if(x==2){
+                alumno* alum=new alumno(mat);
+                alum->show();
+                close();
+            } else {
+                close();
+                profe* prof=new profe(mat);
+                prof->show();
+                close();
+            }
     }
 }
